28adc067e1ec3acfb4c6b44e4d6681335b64fce1
arrowira <41136800+arrowira@users.noreply.github.com>
2025-11-21 14:12:01 -0800

    playerMovementWithDamping

--- mini-jam-there-is-no-end-game/character_body_2d.gd
+++ mini-jam-there-is-no-end-game/character_body_2d.gd
@@ -1,19 +1,20 @@
-extends CharacterBody2D
+extends RigidBody2D
 
 
-const SPEED = 300.0
+@export var SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	var local_dir := Vector2.UP
+	var world_dir := local_dir.rotated(rotation)
 	if Input.is_action_pressed("turn left"):
-		rotate(-PI/180)
+		apply_torque(-3000)
 	if Input.is_action_pressed("turn right"):
-		rotate(PI/180)
+		apply_torque(3000)
+	if Input.is_action_pressed("accelerate"):
+		apply_force(world_dir*SPEED)
+		
 	
 
 	
-	move_and_slide()
