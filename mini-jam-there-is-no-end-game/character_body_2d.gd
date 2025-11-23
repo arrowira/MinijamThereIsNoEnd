5de7ea1f6210cd5a12e09bc072ee79ae307b10d8
arrowira <41136800+arrowira@users.noreply.github.com>
2025-11-22 19:59:09 -0800

    borderDeaths

--- mini-jam-there-is-no-end-game/character_body_2d.gd
+++ mini-jam-there-is-no-end-game/character_body_2d.gd
@@ -36,4 +36,13 @@ func death():
 func _on_death_timer_timeout() -> void:
 	if tutorial:
 		get_tree().change_scene_to_file("res://scenes/tutorial.tscn")
-		
+
+
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	if area.name == "border":
+		death()
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
